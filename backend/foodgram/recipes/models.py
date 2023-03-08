38774d5bfe217f32c9e1d866a0e2b6684e1da7f4
Igor <igor.krupcko2010@yandex.ru>
2023-03-08 22:00:59 +0300

    Добавлены модели

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,170 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Наименование тега',
+        max_length=200,
+        help_text='Укажите наименование тега'
+    )
+    color = models.CharField(
+        'Цвет',
+        max_length=7,
+        null=True,
+        help_text='Укажите цвет тега'
+    )
+    slug = models.SlugField(
+        'Слаг',
+        max_length=200,
+        unique=True,
+        null=True,
+        help_text='Укажите слаг тега'
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        'Наименование ингредиента',
+        max_length=200,
+        help_text='Укажите наименование ингредиента'
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=200,
+        help_text='Укажите единицу измерения ингредиента'
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        'Наименование рецепта',
+        max_length=200,
+        help_text='Укажите наименование рецепта'
+    )
+    text = models.TextField(
+        'Описание рецепта',
+        help_text='Укажите описание рецепта'
+    )
+    image = models.ImageField(
+        'Картинка рецепта',
+        upload_to='recipes/images/',
+        help_text='Добавьте картинку рецепта'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Автор рецепта'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Тег',
+        through='TagRecipe'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингредиент',
+        through='IngredientRecipe'
+    )
+    cooking_time = models.PositiveIntegerField(
+        'Время приготовления',
+        validators=(MinValueValidator(1),),
+        help_text='Укажите время приготовления рецепта (в минутах)'
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+    def __str__(self):
+        return self.name
+
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+    amount = models.PositiveIntegerField(
+        'Количество',
+        validators=(MinValueValidator(1),)
+    )
+
+
+class Favorites(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='favorites',
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь, добавляющий продукты в Избранное'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='favorites',
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт, добавленный в Избранное'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+
+    def __str__(self):
+        return f'{self.user} добавил в Избранное рецепт {self.recipe}'
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='baskets',
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь, добавляющий рецепты в корзину'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        related_name='baskets',
+        on_delete=models.CASCADE,
+        verbose_name='Рецепты, добавленные в Корзину'
+    )
+
+    class Meta:
+        verbose_name = 'Корзина'
+
+    def __str__(self):
+        return f'{self.user} добавил в Корзину рецепт {self.recipe}'
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True,
+        validators=(UnicodeUsernameValidator(),),
+        help_text='Придумайте логин'
+    )
+    email = models.EmailField(
+        'Почта',
+        max_length=254,
+        help_text='Укажите электронную почту'
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150,
+        help_text='Укажите имя'
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150,
+        help_text='Укажите фамилию'
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        related_name='follower',
+        verbose_name='Подписчик',
+        on_delete=models.CASCADE,
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='following',
+        verbose_name='Автор',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=~models.Q(user=models.F('author')),
+                name='user_cannot_follow_yourself'
+            ),
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='unique_following'
+            )
+        ]
+        verbose_name = 'Подписка',
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'{self.user} подписался на {self.author}'
