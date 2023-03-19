1e396bcee48b67f0bfe473f750fe02e82b898f58
Igor <igor.krupcko2010@yandex.ru>
2023-03-19 21:49:53 +0300

    Добавлена Корзина

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,12 @@
+from django.db.models import Sum
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+from rest_framework.status import (HTTP_400_BAD_REQUEST,
+                                   HTTP_201_CREATED,
+                                   HTTP_204_NO_CONTENT
+                                   )
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 
 from .serializers import (IngredientSerializer,
@@ -9,7 +14,13 @@ from .serializers import (IngredientSerializer,
                           RecipeSerializer,
                           ShortRecipeSerializer
                           )
-from recipes.models import Ingredient, Tag, Recipe, Favorites, Basket
+from recipes.models import (Ingredient,
+                            Tag,
+                            Recipe,
+                            Favorites,
+                            Basket,
+                            IngredientRecipe
+                            )
 
 
 class IngredientViewSet(ReadOnlyModelViewSet):
@@ -33,7 +44,30 @@ class RecipeViewSet(ModelViewSet):
 
     @action(detail=False)
     def get_download_shopping_cart(self, request):
-        pass
+        if not Basket.objects.filter(
+                user=request.user
+        ).exists():
+            return Response(
+                {'errors': 'В Корзине отсутствуют рецепты'},
+                status=HTTP_400_BAD_REQUEST
+            )
+        ingredients = IngredientRecipe.objects.filter(
+            recipe__baskets__user=request.user
+        ).values(
+            'ingredient__name',
+            'ingredient__measurement_unit'
+        ).annotate(amount=Sum('amount'))
+        shopping_list = ''
+        for ingredient in ingredients:
+            item = (f'* {ingredient["ingredient__name"]} '
+                    f'({ingredient["ingredient__measurement_unit"]}) -- '
+                    f'{ingredient["amount"]}\n\n'
+                    )
+            shopping_list += item
+        response = HttpResponse(shopping_list,
+                                content_type='text/plain;charset=UTF-8'
+                                )
+        return response
 
     @action(methods=['post', 'delete'], detail=True)
     def shopping_cart(self, request, **kwargs):
@@ -47,8 +81,8 @@ class RecipeViewSet(ModelViewSet):
             if not serializer.is_valid():
                 return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
             if Basket.object.filter(
-                user=request.user,
-                recipe=recipe
+                    user=request.user,
+                    recipe=recipe
             ).exists():
                 return Response(
                     {'errors': 'Данный рецепт уже есть в списке покупок'},
@@ -60,10 +94,15 @@ class RecipeViewSet(ModelViewSet):
             )
             return Response(serializer.data, status=HTTP_201_CREATED)
         if not Basket.objects.filter(
-            user=request.user,
-            recipe=recipe
+                user=request.user,
+                recipe=recipe
         ).exists():
             return Response(
                 {'errors': 'Данный рецепт в Корзине отсутствует'},
                 status=HTTP_400_BAD_REQUEST
             )
+        Basket.objects.filter(
+            user=request.user,
+            recipe=recipe
+        ).delete()
+        return Response(status=HTTP_204_NO_CONTENT)
