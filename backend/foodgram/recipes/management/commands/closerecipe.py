885d54c2cd4e5a56c1d1cfde4c00a46f18c1136b
Igor <igor.krupcko2010@yandex.ru>
2023-03-28 21:53:12 +0300

    Изменение заполнения базы

--- backend/foodgram/recipes/management/commands/closerecipe.py
+++ backend/foodgram/recipes/management/commands/closerecipe.py
@@ -1,5 +1,4 @@
-import os
-from csv import reader
+from csv import DictReader
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
@@ -7,35 +6,18 @@ from django.core.management.base import BaseCommand
 from recipes.models import Ingredient
 
 
-class Command(BaseCommand):
-    help = "Загрузить данные из data/ingredients.csv"
+CSV_FILES = {
+    Ingredient: 'ingredients.csv',
+}
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            'filename',
-            default='ingredients.csv',
-            nargs='?',
-            type=str
-        )
+
+class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print("Загрузка данных из ingredients.csv в Ingredient")
-        if Ingredient.objects.exists():
-            print('Модель ингредиентов заполнена другими данными, '
-                  'отмена загрузки')
-        else:
-            with open(os.path.join(
-                    settings.BASE_DIR,
-                    'data',
-                    options['filename']),
-                    'r',
-                    encoding='utf-8'
-            ) as file_name:
-                data = reader(file_name)
-                for row in data:
-                    name, measurement_unit = row
-                    Ingredient.objects.get_or_create(
-                        name=name,
-                        measurement_unit=measurement_unit
-                    )
-        print("Загрузка завершена")
+        for model, file in CSV_FILES.items():
+            with open(
+                f'{settings.BASE_DIR}/data/{file}',
+                encoding='utf-8'
+            ) as csvfile:
+                for row in DictReader(csvfile):
+                    model.objects.get_or_create(**dict(row))
