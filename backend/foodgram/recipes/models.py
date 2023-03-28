1672e08c56436a386cbd610dadde19714db4d1ee
Igor <igor.krupcko2010@yandex.ru>
2023-03-29 01:54:44 +0300

    Удалена сортировка

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -51,7 +51,7 @@ class Ingredient(models.Model):
     )
 
     class Meta:
-        ordering = ('-id',)
+        # ordering = ('-id',)
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
 
